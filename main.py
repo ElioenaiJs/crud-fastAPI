a116c28761296653fe57cb65d5f05c7a194477d8
Elio <elio.antonio.654@gmail.com>
2025-04-13 13:21:40 -0600

    feat: Agregar modelo de tarea y configuración de base de datos en SQLAlchemy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from models import task 
 
 app = FastAPI()
 
--- models/task.py
+++ models/task.py
@@ -1,11 +1,14 @@
-from sqlalchemy import Boolean, Column, Integer, String, ForeignKey
+from sqlalchemy import Boolean, Column, Integer, String
 from sqlalchemy.orm import declarative_base
+from db import engine
 
 Base = declarative_base()
 
 class Task(Base):
     __tablename__ = 'tasks'
     id = Column(Integer, primary_key=True)
-    title = Column(String, index=True)
-    description = Column(String, index=True)
+    title = Column(String(255), index=True)         
+    description = Column(String(255), index=True)  
     completed = Column(Boolean, default=False)
+
+Base.metadata.create_all(bind=engine)
